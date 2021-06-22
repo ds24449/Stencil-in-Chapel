Corrected Mixed derivative
- The values are now pass by reference using ref intent.
- Works as intended.

PythonScripts/sinXAvgError.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-savedFile = open("Tests\Data\sinAvgError.txt", "r")
+savedFile = open("Stencil-in-Chapel\Tests\Data\sinAvgError.txt", "r")
 li = []
 for line in savedFile:
     li.append(list(map(float, line.split(" "))))
@@ -11,10 +11,10 @@ h_values, error_values = tuple(li)
 plt.yscale('log')
 plt.xscale('log')
 plt.plot(h_values, error_values, '-o', label="error vs H")
-plt.plot(h_values, np.power(h_values, 4), '-x', label="h*h vs h")
+plt.plot(h_values, np.power(h_values, 2), '-x', label="h*h vs h")
 plt.xlabel('h Values')
 plt.ylabel('Avg Error')
 plt.title('Sin(X) Derivative test')
 plt.legend()
-plt.savefig(fname="PythonScripts\sinXAvgErr", dpi=1000)
+# plt.savefig(fname="PythonScripts\sinXAvgErr", dpi=1000)
 plt.show()